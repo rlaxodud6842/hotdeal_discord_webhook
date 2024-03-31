14c09d6f641b10cdc6c42087aa3444b52dcedee4
rlaxodud6842 <rlaxodud6842@naver.com>
2024-04-01 00:06:06 +0900

    기ë아카라이브 알리ë°미로 전향 및 기1시간ë크1시간마다 크롤하는데, 중복시 제거하는 이슈 해결중

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import requests
-import pomppu as p
+import CrwalTool as aca
 
 import schedule
-import time
 
 discord_url = "https://discord.com/api/webhooks/1221802874884063252/c_1frV36Yt2a-qEVLAG6hO66OI-oBL_WAoj4tlsvA0lNZMOp1AGSL_6fxwH3MW1C2QtO"
 # 디스코드 채널로 메세지 전송
 
-pp = p.Pomppu()
-qa = p.Quasar()
-ac = p.Aca()
+ac = aca.Acalive()
 def discord_send_message():
-    message = {"content": f"{pp.get_string() +"\n" + qa.get_string() +"\n" + ac.get_string()}"}
+    message = {"content": f"{ac.get_string()}"}
     requests.post(discord_url, data=message)
 
 discord_send_message()
+
 schedule.every(1).minutes.do(discord_send_message)
 while True:
-    print("반복")
     schedule.run_pending()
 
+
+# 1시간분마다 크롤링.
+# 크롤링에 있는거랑 비교해서 달라진거 있으면 올리고, 없으면 대기
